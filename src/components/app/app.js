af25ab716d70f490b3a9a93afa997d6766720ec2
Kirill Eliseev <ekn62@bk.ru>
2023-03-25 18:22:48 +0300

    add new funck

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,26 +1,32 @@
-
 import AppFilter from '../app-filter/app-filter'
 import AppInfo from '../app-info/app-info'
 import EmployeesAddForm from '../employees-add-form/employees-add-form'
 import EmployeesList from '../employees-list/employess-list'
 import SearchPanel from '../search-panel/search-panel'
+
 import './app.css'
 
 function App() {
-	return (
-		<div className='app'>
-			<AppInfo/>
 
-			<div className="search-panel">
-				<SearchPanel/>
-				<AppFilter/>
-			</div>
-			
-			<EmployeesList/>
+    const data = [
+        {name: 'John C.', salary: 800, increase: false, id: 1},
+        {name: 'Alex M.', salary: 3000, increase: true, id: 2},
+        {name: 'Carl W.', salary: 5000, increase: false, id: 3}
+    ];
 
-			<EmployeesAddForm/>
-		</div>
-	);
+    return (
+        <div className="app">
+            <AppInfo />
+
+            <div className="search-panel">
+                <SearchPanel/>
+                <AppFilter/>
+            </div>
+            
+            <EmployeesList data={data}/>
+            <EmployeesAddForm/>
+        </div>
+    );
 }
 
 export default App;
